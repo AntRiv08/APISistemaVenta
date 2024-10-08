64fde72b41d86cac7066a3cae89b7ca05307fee3
Antonio Rivera <antonioo.rivera24@hotmail.com>
2024-10-08 16:05:00 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVenta.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class GenericRepository<TModel> : IGenericRepository<TModel> where TModel : class
+    {
+        private readonly DbventaContext _context;
+        public GenericRepository(DbventaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro)
+        {
+            try
+            {
+                TModel modelo = await _context.Set<TModel>().FirstOrDefaultAsync(filtro);
+                return modelo;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<TModel> Crear(TModel modelo)
+        {
+            try
+            {
+                _context.Set<TModel>().Add(modelo);
+                await _context.SaveChangesAsync();
+                return modelo;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Editar(TModel modelo)
+        {
+            try
+            {
+                _context.Set<TModel>().Update(modelo);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Eliminar(TModel modelo)
+        {
+            try
+            {
+                _context.Set<TModel>().Remove(modelo);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public async Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null)
+        {
+            try
+            {
+                IQueryable<TModel> queryModel =
+                    filtro == null ? _context.Set<TModel>() : _context.Set<TModel>().Where(filtro);
+                return queryModel;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ SistemaVenta.DAL/Repositorios/VentaRepository.cs
@@ -0,0 +1,65 @@
+﻿using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class VentaRepository : GenericRepository<Venta>, IVentaRepository
+    {
+        private readonly DbventaContext _context;
+
+        public VentaRepository(DbventaContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Venta> Registrar(Venta modelo)
+        {
+            Venta ventaGenerada = new Venta();
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach (DetalleVenta dv in modelo.DetalleVenta)
+                    {
+                        Producto producto_encontrado = _context.Productos.Where(p => p.IdProducto == dv.IdProducto).First();
+                        producto_encontrado.Stock = producto_encontrado.Stock - dv.Cantidad;
+                        _context.Productos.Update(producto_encontrado);
+                    }
+                    await _context.SaveChangesAsync();
+
+                    NumeroDocumento correlativo = _context.NumeroDocumentos.First();
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaRegistro = DateTime.Now;
+                    _context.NumeroDocumentos.Update(correlativo);
+                    await _context.SaveChangesAsync();
+
+                    int cantidadDigitos = 4;
+                    string ceros = string.Concat(Enumerable.Repeat("0", cantidadDigitos));
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - cantidadDigitos, cantidadDigitos);
+                    modelo.NumeroDocumento = numeroVenta;
+                    await _context.Venta.AddAsync(modelo);
+                    await _context.SaveChangesAsync();
+
+                    ventaGenerada = modelo;
+
+                    transaction.Commit();
+
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+                return ventaGenerada;
+            }
+        }
+    }
+}
